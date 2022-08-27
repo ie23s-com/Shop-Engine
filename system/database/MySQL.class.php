c777fdb506196953bb2cd2f593c8df1455a5ee13
ie23s <me@ie23s.com>
2022-08-27 18:30:08 +0300

    @

--- system/database/MySQL.class.php
+++ system/database/MySQL.class.php
@@ -3,5 +3,8 @@
 namespace ie23s\shop\system\database;
 
 class MySQL implements Component {
-    
+
+        public function init();
+        public function load();
+        public function unload();
 }
