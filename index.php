bb9bc794302c3d6a81c7629b87ede12c63f6a524
ie23s <me@ie23s.com>
2022-08-28 00:11:13 +0300

    @

---
+++ index.php
@@ -0,0 +1,7 @@
+<?PHP
+dispay_errors(1);
+define("__SHOP_DIR__", __DIR__ . "/");
+
+require_once(__SHOP_DIR__ . 'vendor/autoload.php');
+
+echo "Hello world!";
---
+++ system/System.class.php
@@ -0,0 +1,8 @@
+<?PHP
+namespace ie23s\shop\system;
+
+require_once "";
+
+class System {
+
+}
