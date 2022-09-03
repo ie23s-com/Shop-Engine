1c71c25893bf98b546e011d6ab44d58a5dc699a1
ie23s <me@ie23s.com>
2022-09-03 22:40:43 +0300

    ISE-29 Создать модуль админки

---
+++ admin/Admin.class.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ie23s\shop\admin;
+
+use ie23s\shop\system\Component;
+use ie23s\shop\system\System;
+
+class Admin extends Component
+{
+    public function __construct(System $system)
+    {
+        parent::__construct($system);
+
+//        if($_GET['admin'] != 'ok') {
+//        }
+
+    }
+    /**
+     * @return void
+     */
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
+
+}
---
+++ admin/pages/Pages.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ie23s\shop\admin\pages;
+
+use ie23s\shop\system\pages\Page;
+
+class Pages extends Page
+{
+
+    public function request(array $request): string
+    {
+        // TODO: Implement request() method.
+    }
+}
