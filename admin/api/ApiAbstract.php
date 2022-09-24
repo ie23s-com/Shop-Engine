4817fcab47213ae49ea0fe74c4e143e0409d8526
ie23s <me@ie23s.com>
2022-09-24 21:46:55 +0300

    ISE-50 Создать API для добавления товара (адм)

--- admin/api/ApiAbstract.php
+++ admin/api/ApiAbstract.php
@@ -55,7 +55,7 @@ abstract class ApiAbstract implements ApiInterface
 
     public function getRequest(string $param)
     {
-        return $this->request[$param];
+        return @$this->request[$param];
     }
 
     public function withCode($code, $text = null) {
